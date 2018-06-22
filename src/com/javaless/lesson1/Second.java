d378328ad68cee53344312b45212a66dfc7a430d
BasarabEugen <40469229+BasarabEugen@users.noreply.github.com>
2018-06-22 12:50:25 +0300

    uptade file .gitignore

--- src/com/javaless/lesson1/Second.java
+++ src/com/javaless/lesson1/Second.java
@@ -2,6 +2,6 @@ package com.javaless.lesson1;
 
 public class Second {
     public static void main(String[] args) {
-        System.out.println("My Names Is Dirt");
+        System.out.println("My Name Is Dirt");
     }
 }
